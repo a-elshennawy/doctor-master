83e022673e3ebde8f0181d7a0a92855db81559e5
shennawy <ahmedshennawy997@gmail.com>
2025-06-04 00:50:24 +0300

    readme

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  // the base option should start with a "/"
+  //  as long as hosting we need to make it only "appType./"
   base: "./",
 });
